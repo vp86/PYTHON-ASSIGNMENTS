f073f263eacbed74bee9c20be6d4865e8b5aa200
vp86 <pammyvignesh@gmail.com>
2019-02-17 14:46:14 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 15 15:50:11 2019
+
+@author: vignesh pillai
+"""
+
+Title= "DESCRIPTION OF SONG"
+song, movie, Maleplayback, Femaleplayback, musicdirector, label= "Merke Merke", "Kandanal Mudal", "Shakar Mhadevan","Sadhanasargam","Yuvan Shankaraja","Starmusic" 
+#movie= "Kanda Naal Mudhal"
+#maleplayback= "Shankar Mahadevan"
+#femaleplayback="sadanasargam"
+#musicdirector="Yuvan Shankaraja"
+#label="starmusic"
+Releaseyear= 2005
+duration= 5.05 
+print(Title)
+print("the song " + song + " is from movie " + movie + " by " + Maleplayback +" & "+ Femaleplayback+"musicdirector")
+print("the movie was released in the year", Releaseyear)
+print("the duration of song: ", duration)
+print(" A creation of "+ label)
+"""for more deatils of the song type Kanda Naak Mudhal tamil movie in google:p"""
